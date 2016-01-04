622f734d0ccee4470b434d81830e9acc25b20964
Adam Sinclair <a.ta.sinclair@gmail.com>
2016-01-04 12:48:21 -0500

    fixed problem

--- tutorials/OS/OS.java
+++ tutorials/OS/OS.java
@@ -1,7 +1,8 @@
 /**
  *  We need to determine the user's operating system since we are dealing 
- *  with file pathnames. 
- *
+ *  with file pathnames. Windows pathnames are different than Unix/Unix-like
+ *  pathnames so this class will help determine how the pathnames will be
+ *  structured. This will only be used once at the beginning of the program.
  */
 
 
@@ -9,7 +10,8 @@ public class OS {
 
 	private static String name = null;
 
-	public static String getOSName() {
+	private static String getOSName() {
+		// If no name is assigned, get our OS name
 		if(name == null) {
 			name = System.getProperty("os.name");
 		}
@@ -38,5 +40,11 @@ public class OS {
 	public static void main(String[] args) {
 		if(isLinux())
 			System.out.println("You are running Linux.");
+		else if(isWindows())
+			System.out.println("You are running Windows.");
+		else if(isMac())
+			System.out.println("You are running Mac.");
+		else
+			System.out.println("Unable to determine your OS.");
 	}
 }
